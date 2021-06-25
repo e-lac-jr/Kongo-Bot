96d4ffaae571f44262f49f98952d38c9812ca8cf
Jayr Lacanlale <jrlacanlale@gmail.com>
2021-06-25 14:17:20 -0700

    added template for deathroll command

---
+++ commands/deathroll.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'deathroll',
+    description: "Starts a new deathroll game in the current channel",
+    async execute(client,message,args,Discord){
+        
+    }
+}
