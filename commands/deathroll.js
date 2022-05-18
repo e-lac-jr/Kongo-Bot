c37bb63199f7fa151d435047825cbf8c3d132a6c
Jayr Lacanlale <jrlacanlale@gmail.com>
2022-05-18 16:46:50 -0700

    worked out plan for how the game will be played

--- commands/deathroll.js
+++ commands/deathroll.js
@@ -6,16 +6,27 @@ module.exports = {
     name: 'deathroll',
     description: "Starts a new deathroll game in the current channel",
     async execute(client,message,args,Discord){
-        counter = 10000;
-        message.channel.send('Game has started!');
-        client.on('message', msg => {
-            if(msg.content === 'roll'){
-                message.channel.send(`${message.author} has rolled down to ` + (counter -= (getRandomInt(counter) + 1)) + '!');
-            }
-            if(counter <= 0) {
-                message.channel.send(`Game Over ${message.author} has died :\(`);
-                return;
-            }
-        })
+        /***
+         * Step 1: Embed pops up
+         * Step 2: Users react to the game to join the game
+         * Step 3: Reaction starts the game
+         * Step 4: Another embed pops up to scramble roll order and give a visual update of the game after every roll
+         * ***/
+        try{
+            counter = 10000;
+            message.channel.send('Game has started!');
+            client.on('message', msg => {
+                if(msg.content === 'roll'){
+                    message.channel.send(`${message.author} has rolled down to ` + (counter -= (getRandomInt(counter) + 1)) + '!');
+                }
+                if(counter <= 0) {
+                    message.channel.send(`Game Over ${message.author} has died :\(`);
+                    return;
+                }
+            })
+        }
+        catch(error){
+            message.channel.send("Error, ",error);
+        }
     }
 }
