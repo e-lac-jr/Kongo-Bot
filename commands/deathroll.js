fb795c38fa0d409731d103f898200a4a405a4062
Jayr Lacanlale <jrlacanlale@gmail.com>
2022-05-18 16:19:38 -0700

    starting work on deathroll again

--- commands/deathroll.js
+++ commands/deathroll.js
@@ -7,16 +7,14 @@ module.exports = {
     description: "Starts a new deathroll game in the current channel",
     async execute(client,message,args,Discord){
         counter = 10000;
-        stop = false;
         message.channel.send('Game has started!');
         client.on('message', msg => {
-            if(counter == 0) {
-                stop = true;
-                message.channel.send('Game Over');
-                counter = 10000;
-            }
-            if(msg.content === 'roll' && stop === false){
+            if(msg.content === 'roll'){
                 message.channel.send(`${message.author} has rolled down to ` + (counter -= (getRandomInt(counter) + 1)) + '!');
+            }
+            if(counter <= 0) {
+                message.channel.send(`Game Over ${message.author} has died :\(`);
+                return;
             }
         })
     }
