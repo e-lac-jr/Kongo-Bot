1e1a1e3aefc271e12163aec6e02fabe5b1af7e80
Jayr Lacanlale <jrlacanlale@gmail.com>
2022-05-19 14:53:53 -0700

    updated node and discord js

--- commands/deathroll.js
+++ commands/deathroll.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageCollector } = require('discord.js');
 const exampleEmbed = new MessageEmbed()
 	.setTitle('Deathroll Game')
 	.setDescription('Deathroll time!!!! If you roll a 0 you\'re out!')
@@ -9,6 +9,7 @@ const exampleEmbed = new MessageEmbed()
 	)
 	.setTimestamp();
 const playerList = [];
+const filter = (reaction) => reaction.emoji.name === '🤑';
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
@@ -16,8 +17,10 @@ function getRandomInt(max) {
 
 async function gatherPlayers(message) {
 	try {
-		message.channel.send('Please react to this message to join the deathroll! \n https://www.gannett-cdn.com/authoring/2006/07/29/NSHT/ghows-LK-de1b4563-79ef-4cbf-8111-79e9784801a2-f8312043.jpeg?crop=799,451,x0,y15&width=799&height=451&format=pjpg&auto=webp')
-			.then(m => m.react('🤑'));
+		message.channel.send('Please react 🤑 to join the deathroll! \n https://www.gannett-cdn.com/authoring/2006/07/29/NSHT/ghows-LK-de1b4563-79ef-4cbf-8111-79e9784801a2-f8312043.jpeg?crop=799,451,x0,y15&width=799&height=451&format=pjpg&auto=webp')
+			.then(m => m.react('🤑'))
+			.then(m => m.createReactionCollector({ filter, time: 15_000 }))
+			.then(c => c.on('collect', r => console.log(`Collected ${r.emoji.name}`)));
 	}
 	catch (error) {
 		message.channel.send('Something failed while gathering players');
@@ -36,7 +39,7 @@ module.exports = {
          * Step 4: Another embed pops up to scramble roll order and give a visual update of the game after every roll
          * ***/
 		try {
-			await gatherPlayers(message);
+			await gatherPlayers(message, client);
 			message.channel.send({ embed: exampleEmbed });
 		}
 		catch (error) {
