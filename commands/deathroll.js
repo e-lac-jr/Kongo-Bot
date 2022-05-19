89890e4749866183d063605f3686befee45fb810
Jayr Lacanlale <jrlacanlale@gmail.com>
2022-05-18 21:58:18 -0700

    setup the embed for the game

--- commands/deathroll.js
+++ commands/deathroll.js
@@ -1,6 +1,20 @@
+const { MessageEmbed } = require('discord.js');
+const exampleEmbed = new MessageEmbed()
+	.setTitle('Deathroll Game')
+	.setDescription('Deathroll time!!!! If you roll a 0 you\'re out!')
+	.setThumbnail('https://cdn.vox-cdn.com/thumbor/xpObSXlKm67r-l1PKp7bVtHSArM=/1400x1050/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/10423639/Screen_Shot_2017_08_29_at_4.27.44_PM.png')
+	.addFields(
+		{ name: 'Player list:', value: '-insert player list here-' },
+		{ name: '\u200B', value: '\u200B' },
+	)
+	.setTimestamp();
+
 function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
 }
+
+// async function gatherPlayers() {
+// }
 
 module.exports = {
 	name: 'deathroll',
@@ -12,11 +26,7 @@ module.exports = {
          * Step 3: Reaction starts the game
          * Step 4: Another embed pops up to scramble roll order and give a visual update of the game after every roll
          * ***/
-		try {
-			message.channel.send('');
-		}
-		catch (error) {
-			message.channel.send('Error, ' + error);
-		}
+		// await gatherPlayers();
+		message.channel.send({ embed: exampleEmbed });
 	},
 };
