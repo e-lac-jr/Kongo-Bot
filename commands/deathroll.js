eddd09b14a17c0ef89cf981e854de94e9c2e5359
Jayr Lacanlale <jrlacanlale@gmail.com>
2022-05-20 14:10:38 -0700

    instead of reacting to a message, players will now react to an embed

--- commands/deathroll.js
+++ commands/deathroll.js
@@ -2,8 +2,11 @@ const { MessageEmbed, MessageCollector } = require('discord.js');
 const exampleEmbed = new MessageEmbed()
 	.setTitle('Deathroll Game')
 	.setDescription('Deathroll time!!!! If you roll a 0 you\'re out!')
-	.setThumbnail('https://cdn.vox-cdn.com/thumbor/xpObSXlKm67r-l1PKp7bVtHSArM=/1400x1050/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/10423639/Screen_Shot_2017_08_29_at_4.27.44_PM.png')
+	.setImage('https://cdn.vox-cdn.com/thumbor/xpObSXlKm67r-l1PKp7bVtHSArM=/1400x1050/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/10423639/Screen_Shot_2017_08_29_at_4.27.44_PM.png')
 	.setTimestamp();
+const gatherPlayerEmbed = new MessageEmbed()
+	.setTitle('Please react 🤑 to join the deathroll! You have 10 seconds to join!!')
+	.setImage('https://www.gannett-cdn.com/authoring/2006/07/29/NSHT/ghows-LK-de1b4563-79ef-4cbf-8111-79e9784801a2-f8312043.jpeg?crop=799,451,x0,y15&width=799&height=451&format=pjpg&auto=webp')
 const playerList = [];
 
 function getRandomInt(max) {
@@ -12,7 +15,7 @@ function getRandomInt(max) {
 
 async function gatherPlayers(message) {
 	try {
-		message.channel.send('Please react 🤑 to join the deathroll! You have 10 seconds to join!! \n https://www.gannett-cdn.com/authoring/2006/07/29/NSHT/ghows-LK-de1b4563-79ef-4cbf-8111-79e9784801a2-f8312043.jpeg?crop=799,451,x0,y15&width=799&height=451&format=pjpg&auto=webp')
+		message.channel.send({ embeds : [gatherPlayerEmbed] }) //'Please react 🤑 to join the deathroll! You have 10 seconds to join!! \n https://www.gannett-cdn.com/authoring/2006/07/29/NSHT/ghows-LK-de1b4563-79ef-4cbf-8111-79e9784801a2-f8312043.jpeg?crop=799,451,x0,y15&width=799&height=451&format=pjpg&auto=webp')
 			.then(m => {
 				m.react('🤑');
 				const filter = (reaction, user) => reaction.emoji.name === '🤑' && !user.bot;
