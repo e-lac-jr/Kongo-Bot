89d8304a376381f10e35418a8456b9a924dfb6c2
Jayr Lacanlale <jrlacanlale@gmail.com>
2022-05-20 13:03:52 -0700

    gather players method working, have to work on embed output

--- commands/deathroll.js
+++ commands/deathroll.js
@@ -3,10 +3,6 @@ const exampleEmbed = new MessageEmbed()
 	.setTitle('Deathroll Game')
 	.setDescription('Deathroll time!!!! If you roll a 0 you\'re out!')
 	.setThumbnail('https://cdn.vox-cdn.com/thumbor/xpObSXlKm67r-l1PKp7bVtHSArM=/1400x1050/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/10423639/Screen_Shot_2017_08_29_at_4.27.44_PM.png')
-	.addFields(
-		{ name: 'Player list:', value: '-insert player list here-' },
-		{ name: '\u200B', value: '\u200B' },
-	)
 	.setTimestamp();
 const playerList = [];
 
@@ -14,12 +10,12 @@ function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
 }
 
-async function gatherPlayers(message, client) {
+async function gatherPlayers(message) {
 	try {
 		message.channel.send('Please react 🤑 to join the deathroll! You have 10 seconds to join!! \n https://www.gannett-cdn.com/authoring/2006/07/29/NSHT/ghows-LK-de1b4563-79ef-4cbf-8111-79e9784801a2-f8312043.jpeg?crop=799,451,x0,y15&width=799&height=451&format=pjpg&auto=webp')
 			.then(m => {
 				m.react('🤑');
-				const filter = (reaction, user) => reaction.emoji.name === '🤑' && user.id != m.author.id;
+				const filter = (reaction, user) => reaction.emoji.name === '🤑' && !user.bot;
 				const collector = m.createReactionCollector({ filter, time: 10000 });
 				collector.on('collect', (reaction, user) => {
 					m.channel.send(`Collected reaction from ${user.tag}`);
@@ -28,7 +24,8 @@ async function gatherPlayers(message, client) {
 				});
 				collector.on('end', () => {
 					m.channel.send('Collection finished!');
-					m.channel.send('Player list: \n ' + playerList);
+					exampleEmbed.addField('Player list:', playerList.toString());
+					m.channel.send({ embeds: [exampleEmbed] });
 				});
 			});
 	}
@@ -45,12 +42,10 @@ module.exports = {
 		/** *
          * Step 1: Message pops up for the start of the game
          * Step 2: Users react to the game to join the game
-         * Step 3: Reaction starts the game
-         * Step 4: Another embed pops up to scramble roll order and give a visual update of the game after every roll
+         * Step 3: Another embed pops up to scramble roll order and give a visual update of the game after every roll
          * ***/
 		try {
-			await gatherPlayers(message, client);
-			message.channel.send({ embeds: [exampleEmbed] });
+			await gatherPlayers(message);
 		}
 		catch (error) {
 			message.channel.send('Something went wrong trying to execute deathroll');
